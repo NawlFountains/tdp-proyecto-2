5eb3c4e1437aa2744c0ffa7a8ecb4f3303377c4d
Agustin Ferrante <agus_p3@hotmail.com>
2021-09-22 17:58:31 -0300

    Implementado constructor de Tetromino.java

--- src/game/tetrominos/Tetromino.java
+++ src/game/tetrominos/Tetromino.java
@@ -1,37 +1,61 @@
 package game.tetrominos;
 
+import game.Grid;
+
 public abstract class Tetromino {
+	
 	protected int[][] shape;
 	protected boolean collided;
-	protected static final int EMPTY=0;
-	protected static final int BLOCK=1;
-	protected static final int CENTROID=2;
 	
-//	public Tetromino(Color color) {
-//		collided=false;
-//	} TODO
+	protected Block centroid;
+	protected Block[] blocks = new Block[3];
+	
+	protected static final int EMPTY = 0;
+	protected static final int BLOCK = 1;
+	protected static final int CENTROID = 2;
+	
+	/**
+	 * Crea un nuevo tetromino con el color dado.
+	 * @param color El color del nuevo tetromino.
+	 */
+	public Tetromino(Color color) {
+		collided = false;
+		centroid = new Block(color);
+		blocks[0] = new Block(color);
+		blocks[1] = new Block(color);
+		blocks[2] = new Block(color);
+	}
+	
 	public void rotateLev() {
 		
 	}
+	
 	public void rotateDext() {
 		
 	}
+	
 	public void fall() {
 		
 	}
+	
 	public boolean collided() {
 		return collided;
 	}
-//	public void setGrid(Grid grid) {
-//		
-//	} TODO
+	
+	public void setGrid(Grid grid) {
+		
+	}
+	
 	public void moveLeft() {
 		
 	}
+	
 	public void moveRight() {
 		
 	}
+	
 	public void notifyGUI(int[][] coordinates) {
 		
 	}
+	
 }
