7ac76cd76d3688b7a9d6be8b878d2e79fe552843
brian <brianclemente@outlook.com>
2021-09-24 13:34:01 -0300

    reparado bug en cell ahora los colores se muestran correctamente

--- src/gui/Cell.java
+++ src/gui/Cell.java
@@ -15,14 +15,14 @@ public class Cell {
  
 	public void setColor(Color c) {
 		switch (c) {
-			case BLUE: emptyCellImagePath="/gui/img/bloques/J.png";
-			case CYAN: emptyCellImagePath="/gui/img/bloques/I.png";
-			case ORANGE: emptyCellImagePath="/gui/img/bloques/L.png";
-			case YELLOW: emptyCellImagePath="/gui/img/bloques/O.png";
-			case GREEN: emptyCellImagePath="/gui/img/bloques/S.png";
-			case PURPLE: emptyCellImagePath="/gui/img/bloques/T.png";
-			case RED: emptyCellImagePath="/gui/img/bloques/Z.png";
-		}
+			case BLUE: emptyCellImagePath="/gui/img/bloques/J.png"; break;
+			case CYAN: emptyCellImagePath="/gui/img/bloques/I.png"; break;
+			case ORANGE: emptyCellImagePath="/gui/img/bloques/L.png"; break;
+			case YELLOW: emptyCellImagePath="/gui/img/bloques/O.png"; break;
+			case GREEN: emptyCellImagePath="/gui/img/bloques/S.png"; break;
+			case PURPLE: emptyCellImagePath="/gui/img/bloques/T.png"; break;
+			case RED: emptyCellImagePath="/gui/img/bloques/Z.png"; break;
+		} 
 	}
 	
 	public String getImagePath() {
