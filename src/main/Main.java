f00a3b18a64f62370c7b2f0d5dff91e717dcf96d
Agustin Ferrante <agus_p3@hotmail.com>
2021-09-23 22:36:19 -0300

    Documentada clase Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,15 @@ package main;
 
 import gui.GUI;
 
+/**
+ * Clase principal del juego.
+ */
 public class Main {
 	
+	/**
+	 * Inicia la aplicacion.
+	 * @param args Argumentos.
+	 */
 	public static void main(String[] args) {
 		GUI ventana = new GUI();
 		ventana.setLocationRelativeTo(null);
