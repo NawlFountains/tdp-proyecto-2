1e60b7e03f45e083651f5bea6b50ad45810767f3
Agustin Ferrante <agus_p3@hotmail.com>
2021-09-25 07:01:53 -0300

    Modificado Cell.java
    modificada la implementacion para que concuerde con el modelo

--- src/gui/Cell.java
+++ src/gui/Cell.java
@@ -1,32 +1,65 @@
 package gui;
 
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import game.tetrominos.Color;
 
-public class Cell {
-	protected String emptyCellImagePath;
+/**
+ * Subclase de JLabel que añade funcionalidad para mostrar un bloque del juego y cambiar su color.
+ */
+@SuppressWarnings("serial")
+public class Cell extends JLabel {
 	
+	protected static final String EMPTY_CELL_IMAGE_PATH = "gui/img/bloques/dummy.png";
+	
+	/**
+	 * Crea una nueva celda con color vacio.
+	 */
 	public Cell() {
-		
+		super();
+		try {
+			this.setIcon(new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream(EMPTY_CELL_IMAGE_PATH))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * Crea una nueva celda con el color pasado como parametro.
+	 * @param c El color de la nueva celda.
+	 */
 	public Cell(Color c) {
 		setColor(c);
 	}
  
+	/**
+	 * Cambia el color de esta celda.
+	 * @param c El nuevo color de la celda.
+	 */
 	public void setColor(Color c) {
-		switch (c) {
-			case BLUE: emptyCellImagePath="/gui/img/bloques/J.png"; break;
-			case CYAN: emptyCellImagePath="/gui/img/bloques/I.png"; break;
-			case ORANGE: emptyCellImagePath="/gui/img/bloques/L.png"; break;
-			case YELLOW: emptyCellImagePath="/gui/img/bloques/O.png"; break;
-			case GREEN: emptyCellImagePath="/gui/img/bloques/S.png"; break;
-			case PURPLE: emptyCellImagePath="/gui/img/bloques/T.png"; break;
-			case RED: emptyCellImagePath="/gui/img/bloques/Z.png"; break;
-		} 
+		String colorPath;
+		if(c != null)
+			colorPath = new StringBuilder("gui/img/bloques/")
+							.append(c.toString().toLowerCase())
+							.append(".png")
+							.toString();
+		else
+			colorPath = EMPTY_CELL_IMAGE_PATH;
+		
+		try {
+			this.setIcon(new ImageIcon(ImageIO.read(getClass().getClassLoader().getResourceAsStream(colorPath))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	@Deprecated
 	public String getImagePath() {
-		return emptyCellImagePath;
+		return null;
 	}
 	
 }
