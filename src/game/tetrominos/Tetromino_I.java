db699851f34048609f450f4cffc95c3eb91ead5b
Agustin Ferrante <agus_p3@hotmail.com>
2021-09-27 22:14:21 -0300

    Modificado Tetromino_I.java
    Arreglado el problema de rotacion del tetromino I cuando esta pegado a un borde de la grilla

--- src/game/tetrominos/Tetromino_I.java
+++ src/game/tetrominos/Tetromino_I.java
@@ -19,4 +19,56 @@ public class Tetromino_I extends Tetromino{
 		shape[0][0] = EMPTY;	shape[1][0] = BLOCK;		shape[2][0] = EMPTY;	shape[3][0] = EMPTY;
 	}
 	
+	@Override
+	public void rotate(int[][] shapeMatrix) {
+		Block[] tetro = getBlocks();
+		
+		int[][] rotatedCollisionCoords = new int[tetro.length][];
+		
+		int[] centroidPosInShape = getCentroidPosInShape(shapeMatrix);
+		int collisionCoordsIndex = 0;
+		boolean rotationSuccess = false;
+		
+		rotatedCollisionCoords[collisionCoordsIndex++] = new int[] {centroid.getX(), centroid.getY()};
+		
+		for(int x = 0; x < shapeMatrix.length; x++)
+			for(int y = 0; y < shapeMatrix[0].length; y++)
+				if(shapeMatrix[x][y] == BLOCK) {
+					int rotatedX = centroid.getX() - centroidPosInShape[0] + x;
+					int rotatedY = centroid.getY() - centroidPosInShape[1] + y;
+					rotatedCollisionCoords[collisionCoordsIndex++] = new int[] {rotatedX, rotatedY};
+				}
+		
+		if( !outOfBounds(rotatedCollisionCoords) && !collidesWith(tetro, blocksFromCoords(rotatedCollisionCoords)) ) {
+			reposition(rotatedCollisionCoords);
+			rotationSuccess = true;
+		} else {
+			shift(rotatedCollisionCoords, -1);
+			if( !outOfBounds(rotatedCollisionCoords) && !collidesWith(tetro, blocksFromCoords(rotatedCollisionCoords)) ) {
+				reposition(rotatedCollisionCoords);
+				rotationSuccess = true;
+			} else {
+				shift(rotatedCollisionCoords, 2);
+				if( !outOfBounds(rotatedCollisionCoords) && !collidesWith(tetro, blocksFromCoords(rotatedCollisionCoords)) ) {
+					reposition(rotatedCollisionCoords);
+					rotationSuccess = true;
+				} else {
+					shift(rotatedCollisionCoords, -3);
+					if( !outOfBounds(rotatedCollisionCoords) && !collidesWith(tetro, blocksFromCoords(rotatedCollisionCoords)) ) {
+						reposition(rotatedCollisionCoords);
+						rotationSuccess = true;
+					} else {
+						shift(rotatedCollisionCoords, 4);
+						if( !outOfBounds(rotatedCollisionCoords) && !collidesWith(tetro, blocksFromCoords(rotatedCollisionCoords)) ) {
+							reposition(rotatedCollisionCoords);
+							rotationSuccess = true;
+						}
+					}
+				}
+			}
+		}
+		if(rotationSuccess)
+			shape = shapeMatrix;
+	}
+	
 }
