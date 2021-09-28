7958f53f377124d43e104111bd68ad28c075929b
Agustin Ferrante <agus_p3@hotmail.com>
2021-09-28 16:24:03 -0300

    Modificado Grid.java
    Quitada la creacion del tetromino actual del constructor de la grilla

--- src/game/Grid.java
+++ src/game/Grid.java
@@ -29,8 +29,6 @@ public class Grid {
 	public Grid(Game game) {
 		this.game = game;
 		blockMatrix = new Block[COLUMNS][ROWS]; //Simula sistema cartesiano de coordeandas
-		fallingTetromino = randomTetromino();
-		fallingTetromino.setGrid(this);
 		nextTetromino = randomTetromino();
 	}
 	
